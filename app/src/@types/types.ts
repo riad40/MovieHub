d3906f553587179dbdcfb77e4d87ff1fa73ab8fb
riad40 <riadabdelaziz40@gmail.com>
2023-03-15 14:29:01 +0100

    create type for the TMDB api response

---
+++ app/src/@types/types.ts
@@ -0,0 +1,20 @@
+export type Movie = {
+    id: number | string
+    title: string
+    poster_path: string
+    backdrop_path: string
+    overview: string
+    release_date: string
+    vote_average: number
+    vote_count: number
+    popularity: number
+    genres: Array<{ id: number; name: string }>
+    runtime: number
+    budget: number
+    revenue: number
+    production_companies: Array<{ id: number; name: string }>
+    production_countries: Array<{ iso_3166_1: string; name: string }>
+    spoken_languages: Array<{ iso_639_1: string; name: string }>
+    status: string
+    tagline: string
+}
